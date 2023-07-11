641d4960af0c3910a9ebd050b7874d7224644917
Tejaswa2611 <118481392+Tejaswa2611@users.noreply.github.com>
2023-07-12 01:49:54 +0530

    first

---
+++ android/app/src/main/kotlin/com/alfred/ai/alfred/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.alfred.ai.alfred
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:alfred/pallete.dart';
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Alfred',
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+        scaffoldBackgroundColor: Pallete.whiteColor,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Pallete.whiteColor,
+        ),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+// feature branch me edit kr rha me 
+// asdkfjnsadj;n
